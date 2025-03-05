b0a5a10ea2bfc621fabb89844adb6dc10cbdff54
blas <126523540+blaso333@users.noreply.github.com>
2025-03-05 15:10:28 -0300

    Revert "name change"
    This reverts commit 15fa980a4344cf1ac2916c24b7fdc4eed10d7207.

--- ChozaGamer.Presentation/Properties/Resources.Designer.cs
+++ ChozaGamer.Presentation/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace OverclockStore.Presentation.Properties {
+namespace ChozaGamer.Presentation.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace OverclockStore.Presentation.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("OverclockStore.Presentation.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ChozaGamer.Presentation.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
